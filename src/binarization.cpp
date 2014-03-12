66430c45d8d4f7fe3c4b90a431088af727ab76b0
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-03-12 16:05:14 +0100

    Initial commit

---
+++ src/binarization.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+
+int main(int argc, char** argv ){
+    if(argc != 2){
+        std::cout << "Usage: binarize <image>" << std::endl;
+        return -1;
+    }
+
+    cv::Mat image;
+    image = cv::imread(argv[1], 1);
+
+    if (!image.data){
+        std::cout << "Invalid image" << std::endl;
+        return -1;
+    }
+
+    cv::namedWindow("Binarized Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Binarized Image", image);
+
+    cv::waitKey(0);
+
+    return 0;
+}
