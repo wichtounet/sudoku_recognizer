e557f097374273ca613d86362ffddc9786b0a75d
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-04-14 11:47:21 +0200

    Fix training images

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -5,6 +5,8 @@
 #include "dbn/dbn.hpp"
 #include "dbn/layer.hpp"
 #include "dbn/conf.hpp"
+#include "dbn/labels.hpp"
+#include "dbn/test.hpp"
 
 #include "detector.hpp"
 #include "data.hpp"
@@ -64,14 +66,17 @@ int main(int argc, char** argv ){
         }
     } else if(command == "train"){
         typedef dbn::dbn<
-            dbn::layer<dbn::conf<true, 10, true, true>, 64 * 64, 30>,
+            dbn::layer<dbn::conf<true, 100, true, true>, 64 * 64, 30>,
             //dbn::layer<dbn::conf<true, 100, false, true>, 300, 300>,
-            dbn::layer<dbn::conf<true, 10, false, true>, 30, 30>,
-            dbn::layer<dbn::conf<true, 10, false, true, true, dbn::Type::EXP>, 30, 10>> dbn_t;
+            dbn::layer<dbn::conf<true, 100, false, true>, 30, 30>,
+            dbn::layer<dbn::conf<true, 100, false, true, true, dbn::Type::EXP>, 30, 10>> dbn_t;
 
         auto dbn = std::make_unique<dbn_t>();
 
         dbn->display();
+
+        std::vector<vector<double>> training_images;
+        std::vector<char> labels;
 
         for(size_t i = 2; i < static_cast<size_t>(argc); ++i){
             std::string image_source_path(argv[i]);
@@ -87,10 +92,55 @@ int main(int argc, char** argv ){
 
             auto data = read_data(image_source_path);
 
+            for(size_t i = 0; i < 9; ++i){
+                for(size_t j = 0; j < 9; ++j){
+                    labels.push_back(data.results[i][j]);
+                }
+            }
+
             cv::Mat dest_image;
             auto cells = detect_grid(source_image, dest_image);
             auto mats = split(source_image, cells);
+
+            for(auto& mat : mats){
+                vector<double> image(64 * 64);
+
+                for(size_t i = 0; i < static_cast<size_t>(mat.rows); ++i){
+                    for(size_t j = 0; j < static_cast<size_t>(mat.cols); ++j){
+                        auto value_c = mat.at<unsigned char>(i, j);
+
+                        double value_d;
+                        if(value_c == 255){
+                            value_d = 0.0;
+                        } else {
+                            assert(value_c == 0);
+                            value_d = 1.0;
+                        }
+
+                        image[i * mat.cols + j] = value_d;
+                    }
+                }
+
+                training_images.emplace_back(std::move(image));
+            }
         }
+
+        auto training_labels = dbn::make_fake(labels);
+
+        std::cout << "Start pretraining" << std::endl;
+        dbn->pretrain(training_images, 5);
+
+        std::cout << "Start fine-tuning" << std::endl;
+        dbn->fine_tune(training_images, training_labels, 1, 1000);
+
+        std::ofstream os("dbn.dat", std::ofstream::binary);
+        dbn->store(os);
+
+        std::cout << training_images.size() << " images" << std::endl;
+        std::cout << training_labels.size() << " fake_labels" << std::endl;
+
+        auto error_rate = dbn::test_set(dbn, training_images, labels, dbn::predictor());
+        std::cout << "\tError rate (normal): " << 100.0 * error_rate << std::endl;
     } else {
         std::cout << "Invalid command \"" << command << "\"" << std::endl;
         return -1;
